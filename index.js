8e2a9d085b8818ff8bb44295942984521c0316c2
bryankiriama <bryankiriama@gmail.com>
2025-09-21 08:50:59 +0300

    completed the lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+
+const championName = 'Emmanuel Nyaanga';
+newHeader.textContent = `${championName} is the champion`;
+
+
+document.body.appendChild(newHeader);
